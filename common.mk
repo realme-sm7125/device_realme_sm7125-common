64c82404d913af5bd38a06b53add8e9c07eae1c9
ean-Pierre Rasquin <yank555.lu@gmail.com>
2016-10-01 10:02:52 +0200

    RMX1921: Don't compile SystemUITests
    Change-Id: Ib5114a12ecf2c6c3918a7d99867913946449a97e
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -379,6 +379,9 @@ PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
+# SystemUI
+EXCLUDE_SYSTEMUI_TESTS := true
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
