7a0436fe4f0918c51c407457e3ad3edd89afb9ec
LuK1337 <priv.luk@gmail.com>
2021-04-14 14:29:58 +0200

    sm7125-common: Import QTI vibrator HAL
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -385,7 +385,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm6250
 
 # VNDK
 PRODUCT_PACKAGES += \
