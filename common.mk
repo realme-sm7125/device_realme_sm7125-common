5f0717e381225606904e51e73aec18d7c1af2727
Omar Hamad <etahamad@icloud.com>
2021-10-31 17:06:37 +0000

    sm7125-common: Build libtinyxml.so
    F linker  : CANNOT LINK EXECUTABLE /system/vendor/bin/mm-pp-dpps: library libtinyxml.so not found
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -494,3 +494,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5 \
     android.hardware.radio@1.2 \
     android.hardware.radio.config@1.1 \
+
+# TinyXML
+PRODUCT_PACKAGES += \
+    libtinyxml
