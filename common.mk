35e38bcee235a7d3f509c356097896d11bb8a589
Mayur <ultramayur123@gmail.com>
2021-11-20 13:23:26 +0000

    sm7125-common: remove APEX
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product-if-exists, vendor/realme/sm7125-common/sm7125-common-vendor.mk)
 
 # Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
