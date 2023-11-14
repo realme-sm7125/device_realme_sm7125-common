29bd29060557cfa899c6f3886ed8db4ad29c475d
Akash Srivastava <akashniki@gmail.com>
2023-11-14 09:27:15 +0530

    sm7125-common: Build some missing LDAC libs * Bluetooth libs look for them
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -76,6 +76,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
+    libldacBT_abr \
+    libldacBT_bco \
+    libldacBT_enc \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
