00e225fea9187f5b20fc5613cd27427e413a5678
Mayur <ultramayur123@gmail.com>
2023-12-07 13:52:19 +0530

    sm7125-common: papps: add prebuilt aosp package installer  also convert Android.mk to Android.bp
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- papps/Android.mk
+++ papps/Android.mk
@@ -1,12 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
-#GCamGo
+# GCamGo
 include $(CLEAR_VARS)
 LOCAL_MODULE := GCamGo
 LOCAL_SRC_FILES := priv-app/GCamGo/GCamGo.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_OVERRIDES_PACKAGES := Snap Camera2 GrapheneCamera
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_DEX_PREOPT := false
+include $(BUILD_PREBUILT)
+
+# Aosp package installer
+include $(CLEAR_VARS)
+LOCAL_MODULE := AospCPackageInstaller
+LOCAL_SRC_FILES := priv-app/AospCPackageInstaller/AospCPackageInstaller.apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_OVERRIDES_PACKAGES := PackageInstaller GooglePackageInstaller
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_DEX_PREOPT := false
 include $(BUILD_PREBUILT)
