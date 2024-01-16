0265f795a5bd4bea36b16fdeefdf17be4a57a062
Mayur <ultramayur123@gmail.com>
2024-01-16 12:00:56 +0530

    sm7125-common: remove packagee: add some packages
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- removePackages/Android.mk
+++ removePackages/Android.mk
@@ -5,6 +5,37 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    Camera2 \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PlayAutoInstallConfig \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
     YouTube \
     YTMusic \
     YouTubeMusicPrebuilt \
