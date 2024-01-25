01dae8c01a747429f4833be00d0dfc39caaf4fdf
Mayur <ultramayur123@gmail.com>
2024-01-25 21:47:36 +0530

    sm7125-common: removePackages: revisit  reareange and add some packages
    Co-authored-by: yograjfire18 <yograjfire18@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- removePackages/Android.mk
+++ removePackages/Android.mk
@@ -5,15 +5,29 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    AdaptiveVPNPrebuilt \
+    AiWallpapers \
+    AmbientStreaming \
+    AmbientSensePrebuilt \
+    AppDirectedSMSService \
+    BetterBugStub \
+    CarrierLocation \
+    CarrierMetrics \
+    CarrierWifi \
+    CbrsNetworkMonitor \
+    ConfigUpdater \
     Camera2 \
     ConnMO \
-    ConnMetrics \
     DCMO \
     DevicePolicyPrebuilt \
     DMService \
     Drive \
     GCS \
     GoogleCamera \
+    GoogleFeedback \
+    KidsSupervisionStub \
+    LocationHistoryPrebuilt \
+    MaestroPrebuilt \
     Maps \
     Music \
     MyVerizonServices \
@@ -23,29 +37,36 @@ LOCAL_OVERRIDES_PACKAGES := \
     OemDmTrigger \
     OPScreenRecord \
     Ornament \
+    PixelLiveWallpaperPrebuilt \
+    PixelSupportPrebuilt \
+    PixelWallpapers2020 \
+    PixelWallpapers2021 \
+    PixelWallpapers2022 \
+    PixelWallpapers2022a \
+    PixelWallpapers2023Tablet \
     PlayAutoInstallConfig \
+    RecorderPrebuilt \
     SafetyHubPrebuilt \
+    SafetyRegulatoryInfo \
     SCONE \
     ScribePrebuilt \
     Showcase \
     Snap \
     Snap2 \
     SnapdragonCamera \
+    SoundAmplifierPrebuilt \
     SprintDM \
     SprintHM \
     Tycho \
+    TipsPrebuilt \
     USCCDM \
     Videos \
+    VZWAPNLib \
+    VzwOmaTrigger \
+    WallpapersBReel2020 \
     YouTube \
-    YTMusic \
     YouTubeMusicPrebuilt \
-    SoundAmplifierPrebuilt \
-    PixelLiveWallpaperPrebuilt \
-    PixelWallpapers2020 \
-    PixelWallpapers2021 \
-    PixelWallpapers2022 \
-    PixelWallpapers2022a \
-    PixelWallpapers2023Tablet
+    YTMusic
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
